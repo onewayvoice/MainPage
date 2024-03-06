080f4dce0712bde4fa8e64ff55184a5a394c7aef
zelgerr <matthias.zelger_2404@snets.it>
2024-03-06 15:39:14 +0100

    - Dateien verschoben, allgemein sortiert - navbar in separate CSS Datei verschoben (bei allen gleich)
    Teampage.php
    - Begonnen mit erster Box (Unternehmenswerte)
    
    Tested-by: zelgerr <matthias.zelger_2404@snets.it>

---
+++ Mainpage/MainpageWebsites/Mainpage.php
@@ -0,0 +1,69 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Golfomobilmeister</title>
+    <link rel="icon" href="../../Navbar/NavbarBilder/logo_bild.jpg">
+    <link rel="stylesheet" href="../MainpageCSS/Mainpage.css">
+    <link rel="stylesheet" href="../../Navbar/NavbarCSS/navbar.css">
+
+</head>
+
+<body>
+    <div class="nav-bar">
+        <div class="logo"><!-- logo eini tian -->
+            <a href="Mainpage.php">
+                <img id="nav-bar-logo" src="../MainpageBilder/nav_bar_logo.PNG" alt="Eine illustration eines Golfautos. Darunter steht noch das Wort Car.">
+            </a>
+        </div>
+        <div class="menu-punkte"><!-- navigations punkte -->
+            <a href="">Golfcar</a>
+            <a href="">Sponsor</a>
+            <a href="">Team</a>
+            <a href="" target="_blank">Tagebuch</a>
+        </div>
+    </div>
+    <div id="container">
+
+        <div class="title-bar"> <!-- titel mini text & erstes bild drinnen -->
+            <div class="title">
+                <p id="pre-title">WELCOME TO THE PROJECT</p>
+                <p id="title">The Golf Cart<br>REBELLION</p>
+                <p id="title-text">
+                    In a world where chaos ruled, golf carts united, wheeling through greens, streets, and malls alike.
+                    With their silent motors and leisurely pace, they conquered hearts and spaces, reigning as the unlikely
+                    heroes of transportation grace. So, let's putt-putt our way to a smoother world, one cart at a time!
+                </p>
+                <div id="button-div">
+                    <button id="title-button">Get in Touch</button>   <!-- um später zum kontakt formular zu kommen / maybee socials zu verlinken -->
+                </div>
+            </div>
+            <div class="title-img-div">
+                <img id="title-img" src="../MainpageBilder/preview_hochkant.jpg" alt="text">
+            </div>
+        </div>
+
+        <div id="info-project">
+            <p id="info-project-title">The Journey into the Unknown:<br>A Glimpse Behind the Scenes of Autonomous Golf</p>
+            <p id="info-projct-text">
+                This website is the product of an incredible challenge undertaken by a small group of pioneers: They
+                were tasked with building a self-driving car capable of finding a golf ball and skillfully maneuvering
+                it into a hole. Sounds exciting, doesn't it? Well, that's exactly what it was!
+
+                This platform is not only a showcase for their project but also a place where tech enthusiasts can come
+                together to chat about the fascinating world of autonomous driving and robotics. From the highs to the
+                lows, from the victories to the setbacks - everything is shared here.
+
+                So, dive in, become part of this exciting journey, and let yourself be inspired by the boundless
+                creativity and unwavering enthusiasm of this group! Who knows, you might even discover your own passion
+                for technology and innovation along the way.
+            </p>
+        </div>
+    </div>
+
+<main>
+
+</main>
+<script src="../MainpageJS/Mainpage.js"></script>
+</body>
+</html>
--- Teampage/TeampageWebsites/Teampage.php
+++ Teampage/TeampageWebsites/Teampage.php
@@ -3,15 +3,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Golfomobilmeister</title>
-    <link rel="icon" href="logo_bild.jpg">
-    <link rel="stylesheet" href="MainpageCSS/Mainpage.css">
+    <link rel="icon" href="../../Navbar/NavbarBilder/logo_bild.jpg">
+    <link rel="stylesheet" href="../TeampageCSS/teampage.css">
+    <link rel="stylesheet" href="../../Navbar/NavbarCSS/navbar.css">
+
 </head>
 <body>
 
 <div class="nav-bar">
     <div class="logo"><!-- logo eini tian -->
-        <a href="Mainpage.php">
-            <img id="nav-bar-logo" src="nav_bar_logo.PNG" alt="Eine illustration eines Golfautos. Darunter steht noch das Wort Car.">
+        <a href="../../Mainpage/MainpageWebsites/Mainpage.php">
+            <img id="nav-bar-logo" src="../../Mainpage/MainpageBilder/nav_bar_logo.PNG" alt="Eine illustration eines Golfautos. Darunter steht noch das Wort Car.">
         </a>
     </div>
     <div class="menu-punkte"><!-- navigations punkte -->
@@ -21,8 +23,13 @@
         <a href="" target="_blank">Tagebuch</a>
     </div>
 
-
-
 </div>
+
+
+<div class="begin">
+    <h2> Unsere Unternehmenswerte </h2>
+</div>
+
+
 </body>
 </html>
