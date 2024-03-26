4d3d7225f978557b8c6f74a76bcddb6ccb1119e9
miguel <stgasmig@bx.fallmerayer.it>
2024-03-26 08:33:59 +0100

    schmutz

--- Mainpage/Mainpage.php
+++ Mainpage/Mainpage.php
@@ -1,15 +1,17 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Golfomobilmeister</title>
-    <link rel="icon" href="../Navbar/NavbarBilder/logo_bild.jpg">
-    <link rel="stylesheet" href="MainpageCSS/Mainpage.css">
-    <link rel="stylesheet" href="../Navbar/NavbarCSS/navbar.css">
+</footer>
+<div>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Golfomobilmeister</title>
+        <link rel="icon" href="../Navbar/NavbarBilder/logo_bild.jpg">
+        <link rel="stylesheet" href="MainpageCSS/Mainpage.css">
+        <link rel="stylesheet" href="../Navbar/NavbarCSS/navbar.css">
 
-</head>
+    </head>
 
-<body>
+    <body>
     <div class="nav-bar">
         <div class="logo"><!-- logo eini tian -->
             <a href="Mainpage.php">
@@ -110,43 +112,44 @@
 
     </div>
 
-</body>
-<footer>
-    <div class="footer-content">
-        <div class="footer-section about">
-            <h3>About Us</h3>
-            <p>Wir sind eine kleine Gruppe von vier Enthusiasten, die sich zusammengeschlossen haben, um etwas Neues zu
-                schaffen. Obwohl wir alle keine Erfahrung in dieser speziellen Branche haben, sind wir entschlossen,
-                alles Mögliche zu versuchen und dabei eine Menge zu lernen.
+    </body>
+    <footer>
+        <div class="footer-content">
+            <div class="footer-section about">
+                <h3>About Us</h3>
+                <p>Wir sind eine kleine Gruppe von vier Enthusiasten, die sich zusammengeschlossen haben, um etwas Neues zu
+                    schaffen. Obwohl wir alle keine Erfahrung in dieser speziellen Branche haben, sind wir entschlossen,
+                    alles Mögliche zu versuchen und dabei eine Menge zu lernen.
 
-                Unser Team setzt sich aus Menschen zusammen, die sich leidenschaftlich für Herausforderungen engagieren
-                und ständig nach neuen Möglichkeiten suchen, um ihr Wissen zu erweitern. Jeder von uns bringt
-                einzigartige Fähigkeiten und Perspektiven mit, die es uns ermöglichen, kreative Lösungen zu finden und
-                uns in unserem gemeinsamen Ziel zu unterstützen.
+                    Unser Team setzt sich aus Menschen zusammen, die sich leidenschaftlich für Herausforderungen engagieren
+                    und ständig nach neuen Möglichkeiten suchen, um ihr Wissen zu erweitern. Jeder von uns bringt
+                    einzigartige Fähigkeiten und Perspektiven mit, die es uns ermöglichen, kreative Lösungen zu finden und
+                    uns in unserem gemeinsamen Ziel zu unterstützen.
 
-                Wir wissen, dass der Weg vor uns herausfordernd sein wird, aber wir sind bereit, uns den
-                Herausforderungen zu stellen und gemeinsam zu wachsen. Wir glauben an den Wert von hartem Arbeit,
-                Teamarbeit und der Bereitschaft, aus Fehlern zu lernen. Mit jeder Herausforderung, die wir überwinden,
-                kommen wir unserem Ziel näher und werden stärker als Team.
-            </p>
+                    Wir wissen, dass der Weg vor uns herausfordernd sein wird, aber wir sind bereit, uns den
+                    Herausforderungen zu stellen und gemeinsam zu wachsen. Wir glauben an den Wert von hartem Arbeit,
+                    Teamarbeit und der Bereitschaft, aus Fehlern zu lernen. Mit jeder Herausforderung, die wir überwinden,
+                    kommen wir unserem Ziel näher und werden stärker als Team.
+                </p>
+            </div>
+            <div class="footer-section contact">
+                <h3>Contact Us</h3>
+                <p>Email: example@example.com</p>
+            </div>
+            <div class="footer-section links">
+                <h3>Quick Links</h3>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="../Golfocar/Goflcar.php">Golfcar</a></li>
+                    <li><a href="../Teampage/Teampage.php">Team</a></li>
+                    <li><a href="../Tagebuch/Teamspace.php" target="_blank">Tagebuch</a></li>
+                </ul>
+            </div>
         </div>
-        <div class="footer-section contact">
-            <h3>Contact Us</h3>
-            <p>Email: example@example.com</p>
+        <div class="footer-bottom"><!-- &copy; ist das logo mit dem C -->
+            &copy; 2024 MyWebsite | Designed by Me
         </div>
-        <div class="footer-section links">
-            <h3>Quick Links</h3>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="../Golfocar/Goflcar.php">Golfcar</a></li>
-                <li><a href="../Teampage/Teampage.php">Team</a></li>
-                <li><a href="../Tagebuch/Teamspace.php" target="_blank">Tagebuch</a></li>
-            </ul>
-        </div>
-    </div>
-    <div class="footer-bottom"><!-- &copy; ist das logo mit dem C -->
-        &copy; 2024 MyWebsite | Designed by Me
-    </div>
-</footer>
+
+</div>
 <script src="MainpageJS/Mainpage.js"></script>
 </html>
