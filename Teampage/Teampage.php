76490f3604c896faec5d92bf98f121393fdbe721
ghost23k3 <ststodan@bx.fallmerayer.it>
2024-03-27 16:01:56 +0100

    alles angepasst nicht fertig

--- Teampage/Teampage.php
+++ Teampage/Teampage.php
@@ -29,7 +29,7 @@
 <!-- Breadcrumb -->
 <nav class="breadcrumb" id="teamPageBreadCrumb">
     <a href="../Mainpage/Mainpage.php">Startseite</a>
-    <a href="" style="font-weight: bold">Unternehmen</a>
+    <a href="" style="font-weight: bold">&nbsp;Unternehmen</a>
 </nav>
 
 <!-- Unternehmenswerte -->
@@ -88,4 +88,42 @@
 
 
 </body>
+<footer>
+    <div class="footer-content">
+        <div class="footer-section about">
+            <h3>About Us</h3>
+            <p>Wir sind eine kleine Gruppe von vier Enthusiasten, die sich zusammengeschlossen haben, um etwas Neues zu
+                schaffen. Obwohl wir alle keine Erfahrung in dieser speziellen Branche haben, sind wir entschlossen,
+                alles Mögliche zu versuchen und dabei eine Menge zu lernen.
+
+                Unser Team setzt sich aus Menschen zusammen, die sich leidenschaftlich für Herausforderungen engagieren
+                und ständig nach neuen Möglichkeiten suchen, um ihr Wissen zu erweitern. Jeder von uns bringt
+                einzigartige Fähigkeiten und Perspektiven mit, die es uns ermöglichen, kreative Lösungen zu finden und
+                uns in unserem gemeinsamen Ziel zu unterstützen.
+
+                Wir wissen, dass der Weg vor uns herausfordernd sein wird, aber wir sind bereit, uns den
+                Herausforderungen zu stellen und gemeinsam zu wachsen. Wir glauben an den Wert von hartem Arbeit,
+                Teamarbeit und der Bereitschaft, aus Fehlern zu lernen. Mit jeder Herausforderung, die wir überwinden,
+                kommen wir unserem Ziel näher und werden stärker als Team.
+            </p>
+        </div>
+        <div class="footer-section contact">
+            <h3>Contact Us</h3>
+            <p>Email: example@example.com</p>
+        </div>
+        <div class="footer-section links">
+            <h3>Quick Links</h3>
+            <ul>
+                <li><a href="../Mainpage/Mainpage.php">Home</a></li>
+                <li><a href="../Golfocar/Goflcar.php">Golfcar</a></li>
+                <li><a href="#">Team</a></li>
+                <li><a href="../Tagebuch/Teamspace.php" target="_blank">Tagebuch</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="footer-bottom"><!-- &copy; ist das logo mit dem C -->
+        &copy; 2024 MyWebsite | Designed by Me
+    </div>
+
+</footer>
 </html>
