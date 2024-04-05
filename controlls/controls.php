9dedf6e1b10ceaf84264c0261b949f0c28769fba
david <sterldav@bx.fallmerayer.it>
2024-04-05 08:28:11 +0200

    bei controls anmeldung changen und andere seite verlinken

--- controlls/controls.php
+++ controlls/controls.php
@@ -12,49 +12,13 @@
         <div id="loginForm">
             <h2>Login</h2>
             <form id="formLogin">
-                <input type="text" id="username" name="username" placeholder="Benutzername">
-                <input type="password" id="password" name="password" placeholder="Passwort">
+                <label for="username"></label><input type="text" id="username" name="username" placeholder="Benutzername">
+                <label for="password"></label><input type="password" id="password" name="password" placeholder="Passwort">
                 <button type="submit">Anmelden</button>
             </form>
         </div>
-
-        <div id="userEntries" style="display:none;">
-            <button id="logoutButton" onclick="logout()">Log out</button>
-            <button id="controlButton" onclick="toggle_Teamspace()">Control</button>
-            <h2 id="welcomeText">Logged In</h2>
-            <div id="entries"></div>
-
-        </div>
-    </div>
-    <div id="Control-part" style="display: none">
-        <div id="Control-information">
-            <div id="Teamspace-CarInterface">
-                <div id="Control-Car-VideoContainer">
-                    <video id="Teamspace-videoPlayer" controls>
-                        <source src="#" type="video/mp4">
-                        Your browser does not support the video tag.
-                    </video>
-                    <div id="fallbackMessage">Cam not currently available</div>
-                </div>
-                <div id="Teamspace-Car-Control">
-                    <div></div>
-                    <button class="Control-Button">W</button>
-                    <div></div>
-                    <button class="Control-Button">A</button>
-                    <button class="Control-Button">S</button>
-                    <button class="Control-Button">D</button>
-                </div>
-            </div>
-            <div id="Teamspace-Settings" style="display: none">
-
-            </div>
-        </div>
-        <div id="Control-Navigation">
-            <button id="logoutButton" onclick="logout()">Log out</button>
-            <button id="controlButton" onclick="toggle_Teamspace()">Diary</button>
-        </div>
     </div>
 </div>
-<script src="../Tagebuch/functions_Teamspace.js"></script>
+<script src="loginControls.js"></script>
 </body>
 </html>
